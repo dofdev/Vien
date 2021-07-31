d2e334e9e98c4a0c08309fa7a80c05170b5fdde7
spatialfree <cadetmerchant@gmail.com>
2021-07-31 14:39:40 -0400

    Core code

--- Assets/Monolith.cs
+++ Assets/Monolith.cs
@@ -10,7 +10,15 @@ using Random = UnityEngine.Random;
 public class Monolith : MonoBehaviour
 {
   public Rig rig;
+  public Player player;
+  public Gem gem;
+  public List<Enemy> enemies = new List<Enemy>();
   public Render render;
+
+  public Vector3 oriel;
+  public Vector3 cursor;
+  public float safeRadius = 0.2f;
+
 
   void Awake()
   {
@@ -20,14 +28,191 @@ public class Monolith : MonoBehaviour
   void Start()
   {
     rig.Start(this);
+
+    player.Start(this);
+    gem.Start(this);
+
     render.Start(this);
   }
 
+  bool playing = true;
   void Update()
   {
     rig.Update();
 
+    if (playing)
+    {
+      player.Update();
+      gem.Update();
+      for (int i = 0; i < enemies.Count; i++)
+      {
+        enemies[i].Update();
+        if (Vector3.Distance(enemies[i].pos, player.pos) < 0.02f)
+        {
+          Debug.Log("game over");
+          // alternative ending is where all the enemies target what spawned them, and phase out
+          playing = false;
+        }
+      }
+    }
+
+
+
     render.Update();
+  }
+
+  public Vector3 OutOfBounds(Vector3 pos)
+  {
+    if (pos.x < -oriel.x / 2)
+    {
+      return Vector3.right;
+    }
+    else if (pos.x > oriel.x / 2)
+    {
+      return Vector3.left;
+    }
+    else if (pos.y < -oriel.y / 2)
+    {
+      return Vector3.up;
+    }
+    else if (pos.y > oriel.y / 2)
+    {
+      return Vector3.down;
+    }
+    else if (pos.z < -oriel.z / 2)
+    {
+      return Vector3.forward;
+    }
+    else if (pos.z > oriel.z / 2)
+    {
+      return Vector3.back;
+    }
+    return Vector3.zero;
+  }
+}
+
+[Serializable]
+public class Player
+{
+  Monolith mono;
+
+  public Vector3 pos;
+  public float followDist = 0.06f;
+
+  public void Start(Monolith mono)
+  {
+    this.mono = mono;
+
+    pos = Vector3.zero;
+  }
+
+  public void Update()
+  {
+    if (Vector3.Distance(mono.cursor, pos) > followDist)
+    {
+      float speed = 0.5f;
+      // slower inside safeRadius
+      if (pos.magnitude < mono.safeRadius)
+      {
+        speed *= 0.5f;
+      }
+      Vector3 newPos = pos + (mono.cursor - pos).normalized * speed * Time.deltaTime;
+      if (mono.OutOfBounds(newPos) == Vector3.zero)
+      {
+        pos = newPos;
+      }
+    }
+  }
+}
+
+[Serializable]
+public class Gem
+{
+  Monolith mono;
+  public Vector3 pos;
+
+  public void Start(Monolith mono)
+  {
+    this.mono = mono;
+
+    Spawn();
+  }
+
+  public void Spawn()
+  {
+    pos = Vector3.zero;
+    while (pos.magnitude < mono.safeRadius)
+    {
+      pos = new Vector3(
+        Random.Range(-mono.oriel.x / 2, mono.oriel.x / 2),
+        Random.Range(-mono.oriel.y / 2, mono.oriel.y / 2),
+        Random.Range(-mono.oriel.z / 2, mono.oriel.z / 2)
+      );
+    }
+  }
+
+  bool held = false;
+  public void Update()
+  {
+    if (!held && Vector3.Distance(mono.player.pos, pos) < 0.04f)
+    {
+      held = true;
+    }
+    if (held)
+    {
+      pos = mono.player.pos;
+
+      if (pos.magnitude < mono.safeRadius)
+      {
+        Enemy enemy = new Enemy();
+        enemy.Start(mono);
+        mono.enemies.Add(enemy);
+        Spawn();
+
+        held = false;
+      }
+    }
+  }
+}
+
+[Serializable]
+public class Enemy
+{
+  Monolith mono;
+
+  public Vector3 pos, dir;
+
+  public void Start(Monolith mono)
+  {
+    this.mono = mono;
+
+    // spawn out, then clamp in
+    pos = Random.rotation * Vector3.forward * mono.oriel.x * 2;
+    pos.x = Mathf.Clamp(pos.x, -mono.oriel.x / 2, mono.oriel.x / 2);
+    pos.y = Mathf.Clamp(pos.y, -mono.oriel.y / 2, mono.oriel.y / 2);
+    pos.z = Mathf.Clamp(pos.z, -mono.oriel.z / 2, mono.oriel.z / 2);
+    dir = Random.rotation * Vector3.forward;
+  }
+
+  public void Update()
+  {
+    // move forward
+    pos += dir * 0.25f * Time.deltaTime;
+    Vector3 normal = mono.OutOfBounds(pos);
+    if (normal != Vector3.zero && Vector3.Angle(normal, dir) > 90)
+    {
+      dir = Vector3.Reflect(dir, normal);
+      // pos += rot * Vector3.forward * 0.25f * Time.deltaTime;
+    }
+    else
+    {
+      // reflect off of safeRadius
+      if (pos.magnitude < mono.safeRadius)
+      {
+        dir = Vector3.Reflect(dir, pos.normalized);
+        pos += dir * 0.25f * Time.deltaTime;
+      }
+    }
   }
 }
 
@@ -39,8 +224,7 @@ public class Rig
   public Camera cam;
   public PhysicalInput lHand, rHand;
 
-  [SerializeField]
-  public InputControl con;
+  public Vector3 offset;
 
   public void Start(Monolith mono)
   {
@@ -53,14 +237,14 @@ public class Rig
     XRHMD hmd = InputSystem.GetDevice<XRHMD>();
     if (hmd != null)
     {
-      cam.transform.position = hmd.centerEyePosition.ReadValue();
+      cam.transform.position = offset + hmd.centerEyePosition.ReadValue();
       cam.transform.rotation = hmd.centerEyeRotation.ReadValue();
     }
 
     XRController lCon = XRController.leftHand;
     if (lCon != null)
     {
-      lHand.pos = lCon.devicePosition.ReadValue();
+      lHand.pos = offset + lCon.devicePosition.ReadValue();
       lHand.rot = lCon.deviceRotation.ReadValue();
 
       lHand.button.Set(lCon.TryGetChildControl("primarybutton").IsPressed());
@@ -75,11 +259,13 @@ public class Rig
     XRController rCon = XRController.rightHand;
     if (rCon != null)
     {
-      rHand.pos = rCon.devicePosition.ReadValue();
+      rHand.pos = offset + rCon.devicePosition.ReadValue();
       rHand.rot = rCon.deviceRotation.ReadValue();
 
       rHand.button.Set(rCon.TryGetChildControl("primarybutton").IsPressed());
     }
+
+    mono.cursor = rHand.pos;
   }
 }
 
@@ -127,7 +313,7 @@ public class Render
   Monolith mono;
 
   public Material matDefault;
-  public Mesh meshCube;
+  public Mesh meshCube, meshOriel, meshWorld, meshGem;
 
   public void Start(Monolith mono)
   {
@@ -136,15 +322,29 @@ public class Render
 
   public void Update()
   {
-    DrawMesh(meshCube, matDefault, mono.rig.lHand.pos, mono.rig.lHand.rot, Vector3.one * 0.06f);
-    DrawMesh(meshCube, matDefault, mono.rig.rHand.pos, mono.rig.rHand.rot, Vector3.one * 0.06f);
+    DrawMesh(meshCube, matDefault, mono.rig.lHand.pos, mono.rig.lHand.rot, Vector3.one * 0.03f);
+    DrawMesh(meshCube, matDefault, mono.rig.rHand.pos, mono.rig.rHand.rot, Vector3.one * 0.03f);
 
+    DrawMesh(meshOriel, matDefault, Vector3.zero, Quaternion.identity, mono.oriel);
+    DrawMesh(meshWorld, matDefault, Vector3.zero, Quaternion.identity, Vector3.one * mono.safeRadius);
+
+    DrawMesh(meshCube, matDefault, mono.cursor, Quaternion.identity, Vector3.one * 0.02f);
+
+    DrawMesh(meshCube, matDefault, mono.player.pos, Quaternion.identity, Vector3.one * 0.02f);
+
+    DrawMesh(meshGem, matDefault, mono.gem.pos, Quaternion.identity, Vector3.one * 0.02f);
+
+    for (int i = 0; i < mono.enemies.Count; i++)
+    {
+      DrawMesh(meshCube, matDefault,
+        mono.enemies[i].pos, Quaternion.LookRotation(mono.enemies[i].dir), Vector3.one * 0.02f);
+    }
   }
   Matrix4x4 m4 = new Matrix4x4();
 
   void DrawMesh(Mesh mesh, Material mat, Vector3 pos, Quaternion rot, Vector3 scale)
   {
-    m4.SetTRS(pos, rot.normalized, scale); 
+    m4.SetTRS(pos, rot.normalized, scale);
     Graphics.DrawMesh(mesh, m4, mat, 0);
   }
 }
